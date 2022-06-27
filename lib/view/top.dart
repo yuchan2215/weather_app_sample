742caaf77fd286b6692186ee015331e685d69d88
Miyayu <yuchan2215@gmail.com>
2022-06-27 10:47:24 +0900

    [add] トップページの作成

---
+++ lib/view/top.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("お天気アプリ"),
+      ),
+      drawer: Drawer(
+          child: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              child: Text("お天気アプリ"),
+            ),
+            ListTile(
+              title: const Text("APIキーの設定"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("地域の設定"),
+              onTap: () {},
+            )
+          ],
+        ),
+      )),
+      body: Center(child: Text("Hello")),
+    );
+  }
+}
