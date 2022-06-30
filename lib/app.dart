16b25e87da8ab9acb046c5180a9533e131cf3f46
Miyayu <yuchan2215@gmail.com>
2022-06-30 22:37:38 +0900

    [fix] 2回呼び出される不具合を修正

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:weather_app_sample/core/api_key.dart';
 import 'package:weather_app_sample/view/top.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +27,13 @@ class MyApp extends StatelessWidget {
     );
   }
 
+  final _memoizer = AsyncMemoizer();
+
   Future<bool> _load() async {
-    debugPrint("_loadが呼び出されたよ！");
-    await ApiKey.loadStorage(const FlutterSecureStorage());
-    return Future<bool>.value(true);
+    return await _memoizer.runOnce(() async {
+      debugPrint("_loadが呼び出されたよ！");
+      await ApiKey.loadStorage(const FlutterSecureStorage());
+      return Future<bool>.value(true);
+    });
   }
 }
