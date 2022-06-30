0070b2fc5052d0904d823ce40502b741b74abd30
Miyayu <yuchan2215@gmail.com>
2022-06-30 14:39:40 +0900

    [add] トーストを表示する機能を作成

--- lib/view/top.dart
+++ lib/view/top.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:weather_app_sample/component/string_from_dialog.dart';
+import 'package:weather_app_sample/component/toast_widget.dart';
+import 'package:weather_app_sample/model/setting_state.dart';
+import 'package:weather_app_sample/viewmodel/setting_notifier.dart';
 
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({
@@ -36,4 +41,38 @@ class MyHomePage extends ConsumerWidget {
           ),
         ),
       );
+
+  /// APIキーを設定するためのウィンドウを開きます。
+  /// [FToast]を使用します。
+  void showApiKeyConfiguration(
+      FToast fToast, BuildContext context, WidgetRef ref) {
+    getStringFromDialog(
+      context,
+      title: "APIキーの設定",
+      desc: "APIキーを入力してください。",
+    ).then((v) {
+      fToast.removeCustomToast();
+      fToast.removeQueuedCustomToasts();
+      if (v == null || v.isEmpty) {
+        fToast.showToast(
+          child: const ToastWidget(
+            message: "APIキーの編集をキャンセルしました。",
+            backgroundColor: Colors.amber,
+            textColor: Colors.black,
+            icon: Icons.warning_amber,
+          ),
+        );
+      } else {
+        ref.read(settingProvider.notifier).updateApiKey(v);
+        fToast.showToast(
+          child: ToastWidget(
+            backgroundColor: Theme.of(context).primaryColor,
+            textColor: Theme.of(context).primaryTextTheme.bodyText1?.color,
+            message: "APIキーの保存に成功しました。",
+            icon: Icons.check_circle_outline,
+          ),
+        );
+      }
+    });
+  }
 }
